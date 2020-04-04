6ad7cfb6ff5ff1ce01b92967b455d22abbcfacc9
Varun03072005 <62499738+Varun03072005@users.noreply.github.com>
2020-04-04 09:11:56 +0530

    Add files via upload
    change the background by mouse movement

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+var r = 0;
+var g = 50;
+var b=255;
+
+// INITIALIZE ALL YOUR VARIABLES
+// Ideally you would create 3 variables. red blue green or r, g, b
+
+
+function setup(){
+
+  createCanvas(1200,400);
+}
+
+
+function draw(){
+
+  // change the value of Red based on the mouse movement about the X axis
+  r = mouseX;
+  r = map(r,0,1200,0,255);
+  
+  // change the value of Green based on the mouse movement about the X axis
+  g = mouseX;
+  g = map(g,100,900,0,255);
+   
+  // change the value of Blue based on the mouse movement about the X axis
+  b = mouseX;
+  b = map(b,0,1200,255,0);
+
+  // Use the map() function to do so. 
+
+  // Pass the values to the background() function you have learnt previously.
+  background(r,g,b);
+
+  // Create an ellipse that will move around with you mouse about the X axis.ellipse(x,y,xradius,yradius)
+  // Remember to fill the canvas with white paint before creating the ellipse.fill("white")
+  fill("white");
+  ellipse(mouseX,mouseY,30,30);
+}
